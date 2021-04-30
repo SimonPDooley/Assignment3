c6ab472a783b5005ec1f5abb9de2121306be8a6a
SimonPDooley <82002312+SimonPDooley@users.noreply.github.com>
2021-04-30 12:16:07 -0600

    Create UserService.java

---
+++ UserService.java
@@ -0,0 +1,10 @@
+
+public class UserService {
+	public User createUser(String username, String password, String name) {
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		user.setName(name);
+		return user;
+	}
+}
